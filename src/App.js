829be4bb295b3a12587cb13dac2a36097a8d3595
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-07-27 10:50:28 +0530

    add dummy text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Test sandbox</h1>
       <h2>This is just to practice git-branching in codesandbox</h2>
       <p>Hellew I am sourabh 🐶</p>
+      <p>dummy text</p>
     </div>
   );
 }
